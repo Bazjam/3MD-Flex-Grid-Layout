d5086efc63db2da6245881001a2812b0e4e4f59a
Bazjam <bonusystem@gmail.com>
2023-02-25 19:54:16 +0200

    Small changes

--- html_ts_boilerplate/src/scripts.ts
+++ html_ts_boilerplate/src/scripts.ts
@@ -2,6 +2,6 @@ const burger = document.querySelector('#burger');
 const navMobile = document.querySelector('#nav-mobile');
 
 burger.addEventListener('click',function(){
-    burger.classList.toggle('open');  // toggle add / remove class html
+    burger.classList.toggle('open');
     navMobile.classList.toggle('show');
 });
